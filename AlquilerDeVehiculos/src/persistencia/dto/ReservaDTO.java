e3db48a50b2348750caf23a45b901ee4d2add1d9
erdorath <loisrgil@gmail.com>
2018-12-14 22:41:45 +0100

    Finalizado javadoc hasta DTO.

--- AlquilerDeVehiculos/src/persistencia/dto/ReservaDTO.java
+++ AlquilerDeVehiculos/src/persistencia/dto/ReservaDTO.java
@@ -2,6 +2,10 @@ package persistencia.dto;
 
 import java.time.LocalDateTime;
 
+/**
+ * @author Lois
+ * Data transfer object de Reserva. Utilizada para generar las tablas SQL.
+ */
 public class ReservaDTO {
 	private int id;
 	private LocalDateTime fechaRecogida;
@@ -12,6 +16,17 @@ public class ReservaDTO {
 	private int idSucursalRecogida;
 	private int idSucursalDevolucion;
 	
+	/**
+	 * El constructor de Reserva.
+	 * @param id ID de la entrega.
+	 * @param fechaRecogida Fecha de recogida del coche.
+	 * @param fechaDevolucion Fecha de devolución del coche
+	 * @param modalidadAlquiler Modalidad de alquiler del coche, puede 0 = por km, 1 = ilimitado.
+	 * @param dniCliente Dni del cliente que realiza la reeserva.
+	 * @param nombreCategoria Nombre de la categoría que desea el cliente.
+	 * @param idSucursalRecogida Id de la sucursal en la que se recoge el vehículo.
+	 * @param idSucursalDevolucion Id de la sucursal en la que se devuelve el vehículo.
+	 */
 	public ReservaDTO(int id, LocalDateTime fechaRecogida,
 			LocalDateTime fechaDevolucion, int modalidadAlquiler,
 			String dniCliente, String nombreCategoria, int idSucursalRecogida,
@@ -26,51 +41,115 @@ public class ReservaDTO {
 		setIdSucursalDevolucion(idSucursalDevolucion);
 	}
 
+	/**
+	 * Obtiene el ID de la reserva.
+	 * @return ID de reserva.
+	 */
 	public int getId() {
 		return id;
 	}
+	/**
+	 * Establece el ID de la reserva.
+	 * @param id El nuevo ID de la reserva.
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	/**
+	 * Obtiene la fecha de recogida del coche establecido en la reserva.
+	 * @return La fecha ded recogida.
+	 */
 	public LocalDateTime getFechaRecogida() {
 		return fechaRecogida;
 	}
+	/**
+	 * Establece una nueva fecha de recogida para la reserva.
+	 * @param fechaRecogida La nueva fecha de recogida.
+	 */
 	public void setFechaRecogida(LocalDateTime fechaRecogida) {
 		this.fechaRecogida = fechaRecogida;
 	}
+	/**
+	 * Obtiene la fecha de devolución del vehículo.
+	 * @return La susodicha fecha de devolución.
+	 */
 	public LocalDateTime getFechaDevolucion() {
 		return fechaDevolucion;
 	}
+	/**
+	 * Establece una fecha de devolución para el vehículo.
+	 * @param fechaDevolucion La nueva fecha de decolución.
+	 */
 	public void setFechaDevolucion(LocalDateTime fechaDevolucion) {
 		this.fechaDevolucion = fechaDevolucion;
 	}
+	/**
+	 * Obtiene la modalidad de alquiler del vehículo en esta reserva.
+	 * @return La modalidad de alquiler, 0 = Por km, 1 = ilimitado
+	 */
 	public int getModalidadAlquiler() {
 		return modalidadAlquiler;
 	}
+	/**
+	 * Establecec la modadlidad de alquiler del vehículo.
+	 * @param modalidadAlquiler La modalidad de alquiler, 0 = por km, 1= ilimitado
+	 */
 	public void setModalidadAlquiler(int modalidadAlquiler) {
 		this.modalidadAlquiler = modalidadAlquiler;
 	}
+	/**
+	 * Obtiene el DNI del cliente que realiza la reserva.
+	 * @return El DNI del cliente.
+	 */
 	public String getDniCliente() {
 		return dniCliente;
 	}
+	/**
+	 * Establece el DNI del cliente que realiza la reserva.
+	 * @param dniCliente El dni del cliente.
+	 */
 	public void setDniCliente(String dniCliente) {
 		this.dniCliente = dniCliente;
 	}
+	/**
+	 * Obtiene el nombre de la categoría de reserva
+ 	 * @return El nombre de la categoría.
+	 */
 	public String getNombreCategoria() {
 		return nombreCategoria;
 	}
+	/**
+	 * Establece el nombre de la categoría de reserva.
+	 * @param nombreCategoria El nuevo nombre de categoría.
+	 */
 	public void setNombreCategoria(String nombreCategoria) {
 		this.nombreCategoria = nombreCategoria;
 	}
+	/**
+	 * Obtiene el ID de la sucursal de recogida del coche.
+	 * @return EL ID de la sucursal de recogida.
+	 */
 	public int getIdSucursalRecogida() {
 		return idSucursalRecogida;
 	}
+	/**
+	 * Establece el ID de sucursal de recogida del coche.
+	 * @param idSucursalRecogida El ID de la nueva sucursal de recogida.
+	 */
 	public void setIdSucursalRecogida(int idSucursalRecogida) {
 		this.idSucursalRecogida = idSucursalRecogida;
 	}
+	/**
+	 * Obtiene el ID de la sucursal de devolución del coche.
+	 * @return El ID de la sucursal de devolución.
+	 */
 	public int getIdSucursalDevolucion() {
 		return idSucursalDevolucion;
 	}
+	/**
+	 * Establece el ID de la sucursal de devolución del coche.
+	 * @param idSucursalDevolucion El ID de la nueva sucursal ded devolución.
+	 */
 	public void setIdSucursalDevolucion(int idSucursalDevolucion) {
 		this.idSucursalDevolucion = idSucursalDevolucion;
 	}		
--- AlquilerDeVehiculos/src/persistencia/dto/SucursalDTO.java
+++ AlquilerDeVehiculos/src/persistencia/dto/SucursalDTO.java
@@ -1,10 +1,19 @@
 package persistencia.dto;
 
+/**
+ * @author Lois
+ * Clase Data transfer object de Sucursal. Utilizada para generar las tablas SQL.
+ */
 public class SucursalDTO {
 	private int id;
 	private String direccion;
 	
 	
+	/**
+	 * Constructor de sucursal.
+	 * @param id Id de la sucursal
+	 * @param direccion Dirección física de la sucursal.
+	 */
 	public SucursalDTO(int id, String direccion) {
 		//super();
 		
@@ -12,15 +21,31 @@ public class SucursalDTO {
 		setDireccion(direccion);
 	}
 	
+	/**
+	 * Obtiene el ID de la sucursal.
+	 * @return
+	 */
 	public int getId() {
 		return id;
 	}
+	/**
+	 * Establece el ID de la sucursal.
+	 * @param id El nuevo ID para la sucursal.
+	 */
 	public void setId(int id) {
 		this.id = id;
 	}
+	/**
+	 * Obtiene la dirección de la sucursal.
+	 * @return La dirección de la sucursal.
+	 */
 	public String getDireccion() {
 		return direccion;
 	}
+	/**
+	 * Establece la dirección de la sucursal.
+	 * @param direccion La nueva dirección de la sucursal.
+	 */
 	public void setDireccion(String direccion) {
 		this.direccion = direccion;
 	}
